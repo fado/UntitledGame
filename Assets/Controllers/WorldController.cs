fb13df32f26f628467a0bc8587d1d59e1228d89a
Padraig Donnelly <pdonnelly@runbox.com>
2021-02-15 18:34:43 +0000

    Initial commit.

---
+++ Assets/Controllers/WorldController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+    public Sprite floorSprite;
+    World world;
+
+    // Start is called before the first frame update
+    void Start() {
+        world = new World();
+
+        for(int x = 0; x < world.Width; x++) {
+            for(int y = 0; y < world.Height; y++) {
+                Tile tileData = world.GetTileAt(x, y);
+                
+                GameObject tileGameObject = new GameObject();
+                tileGameObject.name = $"Tile_{x}_{y}";
+                tileGameObject.transform.position = new Vector3(tileData.X, tileData.Y, 0);
+                tileGameObject.AddComponent<SpriteRenderer>();
+
+                tileData.RegisterTileTypeChangedCallback( (tile) => { OnTileTypeChanged(tile, tileGameObject); } );
+            }
+        }
+
+        world.RandomizeTiles();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    /* 
+        Callback function that we set on each tile. Whenever the tile's type changes,
+        this function will be called so that the correct sprite gets set on the 
+        SpriteRenderer.
+     */
+    void OnTileTypeChanged(Tile tileData, GameObject tileGameObject) {
+
+        if(tileData.Type == Tile.TileType.Floor) {
+            tileGameObject.GetComponent<SpriteRenderer>().sprite = floorSprite;
+        } else if (tileData.Type == Tile.TileType.Empty) {
+            tileGameObject.GetComponent<SpriteRenderer>().sprite = null;
+        } else {
+            Debug.LogError("OnTileTypeChanged - Unrecognised tile type.");
+        }
+
+    }
+}
---
+++ Assets/Models/Tile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile {
+    
+    public enum TileType { Empty, Floor };
+
+    private TileType _type = TileType.Empty;
+
+    Action<Tile> callbackTileTypeChanged;
+    
+    World world;
+    public int X { get; protected set; }
+    public int Y { get; protected set; }
+
+    LooseObject looseObject;
+    InstalledObject installedObject;
+
+    public TileType Type {
+        get { return _type; }
+        set {
+            TileType oldType = _type;
+            _type = value;
+
+            if(callbackTileTypeChanged != null && _type != oldType) {
+                callbackTileTypeChanged(this);
+            }
+        }
+    }
+
+    public Tile( World world, int x, int y ) {
+        this.world = world;
+        this.X = x;
+        this.Y = y;
+    }
+
+    public void RegisterTileTypeChangedCallback(Action<Tile> callbackFunction)  {
+        callbackTileTypeChanged += callbackFunction;
+    }
+
+    public void UnregisterTileTypeChangedCallback(Action<Tile> callbackFunction)  {
+        callbackTileTypeChanged -= callbackFunction;
+    }
+
+}
