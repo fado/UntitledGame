7de4acc58d4feec8789cce3a48b171e805bd8ef1
Padraig Donnelly <pdonnelly@runbox.com>
2021-02-16 14:30:34 +0000

    Adds a version of Quill's SimplePool.

--- Assets/Controllers/MouseController.cs
+++ Assets/Controllers/MouseController.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour {
 
-    public GameObject circleCursor;
+    public GameObject circleCursorPrefab;
 
     Vector3 mousePositionLastFrame;
     Vector3 mousePositionCurrentFrame;
     Vector3 dragStartPosition;
 
-    private const int RightMouseButton = 0;
-    private const int LeftMouseButton = 1;
-    private const int MiddleMouseButton = 2;
+    List<GameObject> dragPreviewGameObjects;
+
+    private const int RIGHT_MOUSE_BUTTON = 0;
+    private const int LEFT_MOUSE_BUTTON = 1;
+    private const int MIDDLE_MOUSE_BUTTON = 2;
 
     // Start is called before the first frame update
     void Start() {
-        
+        dragPreviewGameObjects = new List<GameObject>();
     }
 
     // Update is called once per frame
@@ -24,7 +26,7 @@ public class MouseController : MonoBehaviour {
         mousePositionCurrentFrame = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePositionCurrentFrame.z = 0;
 
-        UpdateCursor();
+        //UpdateCursor();
         UpdateDragSelection();
         UpdateCameraMovement();
 
@@ -34,7 +36,7 @@ public class MouseController : MonoBehaviour {
 
     void UpdateCameraMovement() {
         // Handle camera dragging.
-        if(Input.GetMouseButton(LeftMouseButton) || Input.GetMouseButton(MiddleMouseButton)) {
+        if(Input.GetMouseButton(LEFT_MOUSE_BUTTON) || Input.GetMouseButton(MIDDLE_MOUSE_BUTTON)) {
             Vector3 diff = mousePositionLastFrame - mousePositionCurrentFrame;
             Camera.main.transform.Translate(diff);
         }
@@ -42,29 +44,50 @@ public class MouseController : MonoBehaviour {
 
     void UpdateDragSelection() {
         // Start selection drag.
-        if(Input.GetMouseButtonDown(RightMouseButton)) {
+        if(Input.GetMouseButtonDown(RIGHT_MOUSE_BUTTON)) {
             dragStartPosition = mousePositionCurrentFrame;
         }
         
+        int startX = Mathf.FloorToInt(dragStartPosition.x);
+        int endX = Mathf.FloorToInt(mousePositionCurrentFrame.x);
+        int startY = Mathf.FloorToInt(dragStartPosition.y);
+        int endY = Mathf.FloorToInt(mousePositionCurrentFrame.y);
+
+        // If we're dragging down and/or left we need to invert these so that the loop works correctly.
+        if(endX < startX) {
+            int tmp = endX;
+            endX = startX;
+            startX = tmp;
+        }
+        if(endY < startY) {
+            int tmp = endY;
+            endY = startY;
+            startY = tmp;
+        }
+
+        // CLean up old drag previews.
+        while(dragPreviewGameObjects.Count > 0) {
+            GameObject gameObject = dragPreviewGameObjects[0];
+            dragPreviewGameObjects.RemoveAt(0);
+            Destroy(gameObject);
+        }
+
+        if(Input.GetMouseButton(RIGHT_MOUSE_BUTTON)) {
+            // Loop over all the tiles we've selected.
+            for(int x = startX; x <= endX; x++) {
+                for(int y = startY; y <= endY; y++) {
+                    Tile tile = WorldController.Instance.World.GetTileAt(x, y);
+                    if(tile != null) {
+                        // Display the building hint on top of this tile position.
+                        GameObject gameObject = (GameObject)Instantiate(circleCursorPrefab, new Vector3(x, y, 0), Quaternion.identity);
+                        dragPreviewGameObjects.Add(gameObject);
+                    }
+                }
+            }
+        }
+
         // End selection drag.
-        if(Input.GetMouseButtonUp(RightMouseButton)) {
-            int startX = Mathf.FloorToInt(dragStartPosition.x);
-            int endX = Mathf.FloorToInt(mousePositionCurrentFrame.x);
-            int startY = Mathf.FloorToInt(dragStartPosition.y);
-            int endY = Mathf.FloorToInt(mousePositionCurrentFrame.y);
-
-            // If we're dragging down and/or left we need to invert these so that the loop works correctly.
-            if(endX < startX) {
-                int tmp = endX;
-                endX = startX;
-                startX = tmp;
-            }
-            if(endY < startY) {
-                int tmp = endY;
-                endY = startY;
-                startY = tmp;
-            }
-
+        if(Input.GetMouseButtonUp(RIGHT_MOUSE_BUTTON)) {
             // Loop over all the tiles we've selected.
             for(int x = startX; x <= endX; x++) {
                 for(int y = startY; y <= endY; y++) {
@@ -78,17 +101,17 @@ public class MouseController : MonoBehaviour {
         }
     }
 
-    void UpdateCursor() {
-        // Update the circle cursor position.
-        Tile tileUnderMouse = WorldController.Instance.GetTileAtWorldCoord(mousePositionCurrentFrame);
-        if(tileUnderMouse != null) {
-            circleCursor.SetActive(true);
-            Vector3 cursorPosition = new Vector3(tileUnderMouse.X, tileUnderMouse.Y, 0);
-            circleCursor.transform.position = cursorPosition;
-        } else {
-            // Hide the cursor when it's out of range.
-            circleCursor.SetActive(false);
-        }
-    }
+    // void UpdateCursor() {
+    //     // Update the circle cursor position.
+    //     Tile tileUnderMouse = WorldController.Instance.GetTileAtWorldCoord(mousePositionCurrentFrame);
+    //     if(tileUnderMouse != null) {
+    //         circleCursor.SetActive(true);
+    //         Vector3 cursorPosition = new Vector3(tileUnderMouse.X, tileUnderMouse.Y, 0);
+    //         circleCursor.transform.position = cursorPosition;
+    //     } else {
+    //         // Hide the cursor when it's out of range.
+    //         circleCursor.SetActive(false);
+    //     }
+    // }
 
 }
---
+++ Assets/Utils/SimplePool.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Based off this: https://gist.github.com/quill18/5a7cfffae68892621267
+public class SimplePool {
+
+    const int DEFAULT_POOL_SIZE = 3;
+        
+    class Pool {
+        int nextId = 1;
+
+        Stack<GameObject> inactive;
+        GameObject prefab;
+
+        public Pool(GameObject prefab, int initialQuantity) {
+            this.prefab = prefab;
+            // TODO: Figure out if Stack uses linked lists so we can potentially dump this line:
+            inactive = new Stack<GameObject>(initialQuantity);
+        }
+
+        public GameObject Spawn(Vector3 position, Quaternion rotation) {
+            GameObject gameObject;
+
+            if(inactive.Count == 0) {
+                // No objects left in the stack so instantiate a new one.
+                gameObject = (GameObject)GameObject.Instantiate(prefab, position, rotation);
+                gameObject.name = $"{prefab.name}({nextId++})";
+                // Add this custom component so we can link back to this pool.
+                gameObject.AddComponent<PoolMember>().myPool = this;
+            } else {
+                gameObject = inactive.Pop();
+
+                // Guard against possibility that the next inactive object has been destroyed.
+                if(gameObject == null) {
+                    return Spawn(position, rotation);
+                }
+            }
+
+            gameObject.transform.position = position;
+            gameObject.transform.rotation = rotation;
+            gameObject.SetActive(true);
+            return gameObject;
+        }
+
+        public void Despawn(GameObject gameObject) {
+            gameObject.SetActive(false);
+            inactive.Push(gameObject);
+        }
+
+    }
+
+    class PoolMember: MonoBehaviour {
+        public Pool myPool;
+    }
+
+    static Dictionary<GameObject, Pool> pools;
+
+    static void Init(GameObject prefab = null, int quantity = DEFAULT_POOL_SIZE) {
+        if(pools == null) {
+            pools = new Dictionary<GameObject, Pool>();
+        }
+        if(prefab != null && pools.ContainsKey(prefab) == false) {
+            pools[prefab] = new Pool(prefab, quantity);
+        }
+    }
+
+    static public void Preload(GameObject prefab, int quantity = 1) {
+        Init(prefab, quantity);
+
+        GameObject[] objects = new GameObject[quantity];
+        for(int i = 0; i < quantity; i++) {
+            objects[i] = Spawn(prefab, Vector3.zero, Quaternion.identity);
+        }
+        for(int i = 0; i < quantity; i++) {
+            Despawn(objects[i]);
+        }
+    }
+
+    static public GameObject Spawn(GameObject prefab, Vector3 position, Quaternion rotation) {
+        Init(prefab);
+        return pools[prefab].Spawn(position, rotation);
+    }
+
+    static public void Despawn(GameObject gameObject) {
+        PoolMember poolMember = gameObject.GetComponent<PoolMember>();
+        if(poolMember == null) {
+            Debug.Log($"Object {gameObject.name} wasn't spawned from a pool. Destroying it instead.");
+            GameObject.Destroy(gameObject);
+        } else {
+            poolMember.myPool.Despawn(gameObject);
+        }
+    }
+    
+}
